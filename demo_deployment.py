58e7e6505c6ed509fb982a896abf7b956f735d18
Paige Gulley <pgulley@campus-vpn-172-26-66-200.vpn.umass.edu>
2024-02-12 22:21:53 -0500

    manual reqs..

--- demo_deployment.py
+++ demo_deployment.py
@@ -1,12 +1,12 @@
 from prefect import flow
 from prefect.runner.storage import GitRepository
 from prefect.blocks.system import Secret
-from prefect.deployments.steps import pip_install_requirements
+
 from sous_chef import Pipeline, recipe_loader
 
 @flow()
 def RunRecipe(config_location):
-    pip_install_requirements()
+
 
     with open(config_location, "r") as config_yaml:
         json_conf = recipe_loader.yaml_to_conf(config_yaml)
@@ -19,9 +19,12 @@ def RunRecipe(config_location):
     pipeline = Pipeline(json_conf)
     #pipeline.run()
 
+reqs = """matplotlib==3.8.2, mediacloud==4.1.3, mc-providers==0.5.4, wayback-news-search==1.2.*, numpy==1.26.4, pandas==2.2.0, prefect==2.14.20, pydantic==2.6.1, pytest==8.0.0, PyYAML==6.0.1, Requests==2.31.0, transformers==4.37.2, yake==0.4.8"""
+
 
 
 if __name__ == "__main__":
+
     f = flow.from_source(
         source=GitRepository(
             url="https://github.com/mediacloud/sous-chef",
@@ -35,7 +38,9 @@ if __name__ == "__main__":
         cron="0 1 * * *",
         parameters={
             "config_location":"test_yaml/QueryOnlineNewsTest.yaml"
-        }
+        },
+        job_variables={"env": {"EXTRA_PIP_PACKAGES": "boto3"} }
+
     )
 
     
