567551f5921d6821d5ed7101a5af962ccae6b686
Paige Gulley <pgulley@campus-vpn-172-26-66-200.vpn.umass.edu>
2024-02-14 21:43:55 -0500

    draft of s3 export task

---
+++ sous_chef/tasks/export_tasks.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+from ..flowatom import FlowAtom
+from .utils import lazy_import
+from prefect_aws import AwsCredentials
+
+@FlowAtom.register("ExportToS3")
+class ExportToS3(FlowAtom):
+	"""
+	Transfers the file at file_name to an s3 bucker bucket_name
+	"""
+
+	credentials_block:str
+	file_name:str
+	bucket_name:str
+	object_name:str
+
+
+	def task_body(self):
+		aws_credentials = AwsCredentials.load(self.credentials_block)
+		s3_client = aws_credentials.get_boto3_session().client("s3")
+
+		with open(self.file_name, "rb") as f:
+    		s3_client.upload_fileobj(f, self.bucket_name, self.object_name)
