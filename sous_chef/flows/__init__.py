34d61ea95d6698d99f4840e3a7911d1c60d1f163
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2026-01-26 12:25:23 -0500

    new NER tasks

--- sous_chef/flows/__init__.py
+++ sous_chef/flows/__init__.py
@@ -6,7 +6,9 @@ These flows demonstrate the sous-chef architecture with simple examples.
 
 # Import flows to register them
 from .keywords_demo_flow import keywords_demo_flow
+from .entities_demo_flow import entities_demo_flow
 
 __all__ = [
     "keywords_demo_flow",
+    "entities_demo_flow",
 ]
--- sous_chef/tasks/__init__.py
+++ sous_chef/tasks/__init__.py
@@ -6,6 +6,7 @@ Tasks are Prefect-decorated functions that can be used within flows.
 
 from .discovery_tasks import query_online_news
 from .keyword_tasks import extract_keywords
+from .extraction_tasks import extract_entities, top_n_entities
 from .aggregator_tasks import top_n_unique_values
 from .export_tasks import csv_to_b2
 from .email_tasks import send_email, send_templated_email, send_run_summary_email
@@ -13,6 +14,8 @@ from .email_tasks import send_email, send_templated_email, send_run_summary_emai
 __all__ = [
     "query_online_news",
     "extract_keywords",
+    "extract_entities",
+    "top_n_entities",
     "top_n_unique_values",
     "csv_to_b2",
     "send_email",
